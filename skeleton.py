f7d49e5d8c604c6c6f689c7656b5ead52acc8c77
Elijah Obra <eobra8897@sdsu.edu>
2025-02-04 17:48:02 -0800

    Submission folder and readme added

--- skeleton.py
+++ skeleton.py
@@ -37,9 +37,9 @@ class CrewRoster:
 roster=CrewRoster() #Empty Crew roster created
 
     # TODO: Uncomment and implement methods
-    roster.add_member("Alice", "Engineer", 5)
-    roster.add_member("Bob", "Pilot", 10)
-    roster.list_crew()
+roster.add_member("Alice", "Engineer", 5)
+roster.add_member("Bob", "Pilot", 10)
+roster.list_crew()
 
-    roster.remove_member("Alice")
-    roster.list_crew()
+roster.remove_member("Alice")
+roster.list_crew()
